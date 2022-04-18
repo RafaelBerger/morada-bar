5abde3610cb5fc40b27443e3d9f6e84b6cd22be8
Rafael Berger <rafaelberger.dev@gmail.com>
2022-04-18 00:08:37 -0300

    criando cardapio

---
+++ src/components/Cardapio/Cardapio.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./cardapio.css";
+import cardapio1 from "../../assets/images/cardapio-lanche1.png";
+// import cardapio2 from "../../assets/images/cardapio-lanche2.png";
+// import cardapio1 from "../../assets/images/";
+// import cardapio1 from "../../assets/images/";
+// import cardapio1 from "../../assets/images/";
+
+const Cardapio = () => {
+  return (
+    <>
+      <section id="cardapio">
+        <article id="cardapio-bg">
+          <h3>Cardápio</h3>
+          <div>{/* <img src={cardapio1} alt="" /> */}</div>
+        </article>
+      </section>
+    </>
+  );
+};
+
+export default Cardapio;
