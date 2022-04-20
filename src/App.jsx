b5d36b47d7aa4e84123d279b256954c3d5f775dd
Rafael Berger <rafaelberger.dev@gmail.com>
2022-04-20 19:10:01 -0300

    Criando a seção depoimentos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header/Header";
 import Apresentacao from "./components/Apresentacao/Apresentacao";
 import Sobre from "./components/Sobre/Sobre";
 import Cardapio from "./components/Cardapio/Cardapio";
+import Depoimentos from "./components/Depoimentos/Depoimentos";
 import "./index.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Apresentacao />
       <Sobre />
       <Cardapio />
+      <Depoimentos />
     </>
   );
 }
---
+++ src/components/Depoimentos/Depoimentos.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./depoimentos.css";
+
+const Depoimentos = () => {
+  return (
+    <section id="depoimentos">
+      <h5>Depoimentos</h5>
+      <div></div>
+    </section>
+  );
+};
+
+export default Depoimentos;
