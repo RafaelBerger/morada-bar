5515af2b166ea0f23f9a3eca76eafc35048f4d94
Rafael Berger <rafaelberger.dev@gmail.com>
2022-04-24 23:06:45 -0300

    Footer criado e responsivo

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Cardapio from "./components/Cardapio/Cardapio";
 import Depoimentos from "./components/Depoimentos/Depoimentos";
 import Localizacao from "./components/Localizacao/Localizacao";
 import Contato from "./components/Contato/Contato";
+import Footer from "./components/Footer/Footer";
 import "./index.css";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Depoimentos />
       <Localizacao />
       <Contato />
+      <Footer />
     </>
   );
 }
