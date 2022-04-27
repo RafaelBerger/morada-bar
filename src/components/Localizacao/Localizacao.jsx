2a5b867048e0cea49e8e4d81950bafbac0345e00
Rafael Berger <rafaelberger.dev@gmail.com>
2022-04-27 17:44:03 -0300

    Local do mapa trocada para o comércio direto

--- src/components/Localizacao/Localizacao.jsx
+++ src/components/Localizacao/Localizacao.jsx
@@ -12,8 +12,8 @@ const Localizacao = () => {
         </p>
         <div className="localizacao-bg">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3655.078838301861!2d-46.548869549384264!3d-23.637347484569933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce42d18e0b26ad%3A0x34eb6e8be8106ef4!2sR.%20das%20Figueiras%2C%202434%20-%20Jardim%2C%20Santo%20Andr%C3%A9%20-%20SP%2C%2009080-301!5e0!3m2!1spt-BR!2sbr!4v1650912597148!5m2!1spt-BR!2sbr"
-            // loading="lazy"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2584.5308212128125!2d-46.54739025885419!3d-23.63735817835472!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce43e3a91f38ab%3A0x512dd9db517d0ce8!2sMorada%20%7C%20Rango%20e%20Birita!5e0!3m2!1spt-BR!2sbr!4v1651090981963!5m2!1spt-BR!2sbr"
+            loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
             id="mapa"
           ></iframe>
